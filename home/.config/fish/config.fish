0f78e93f1bb8ca1f19fcaeb8fab61bb784565399
市川恭佑 <github@mail.ebi-yade.com>
2023-04-17 17:28:52 +0900

    periodical backup

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+source /Users/ichikawa-kyosuke/.docker/init-fish.sh || true # Added by Docker Desktop
